0333ee3aeb133f23ea071c5d58d4bb0594f2ea90
Fran <franlinaresgorria@gmail.com>
2020-11-23 20:08:16 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntroComponent } from 'src/app/components/intro/intro.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: IntroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
