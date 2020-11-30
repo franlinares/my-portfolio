358d4e84ec64840913d127d3e36181297b4615f5
Fran <franlinaresgorria@gmail.com>
2020-11-30 17:09:32 +0100

    navbar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
