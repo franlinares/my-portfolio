d515efdf5b6c8c81948f05f8fef47306edc6595e
Fran <franlinaresgorria@gmail.com>
2020-12-18 17:24:52 +0100

    cards projects

--- src/app/projects/models/projects.ts
+++ src/app/projects/models/projects.ts
@@ -1,12 +1,18 @@
 export class Projects {
   _id: number;
   name: string;
+  type: string;
+  tech: string;
+  img: string;
   url: string;
   github: string;
 
   constructor(item?: any) {
     this._id = item?._id || 0;
     this.name = item?.name || '';
+    this.type = item?.type || '';
+    this.tech = item?.tech || '';
+    this.img = item?.img || '';
     this.url = item?.url || '';
     this.github = item?.github || '';
   }
