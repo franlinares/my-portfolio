6cc597e593072c41e13830632790ca4550e40050
Fran <franlinaresgorria@gmail.com>
2020-12-27 15:06:53 +0100

    video section done

--- src/app/projects/video/video-cards/video-cards.component.ts
+++ src/app/projects/video/video-cards/video-cards.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VideosService } from 'src/app/projects/services/videos.service';
 import { Video } from 'src/app/projects/models/videos';
+import Swal from 'sweetalert2'
 
 
 
@@ -22,5 +23,41 @@ export class VideoCardsComponent implements OnInit {
         this.videos.push(...resp);
       })
   }
+
+  showVideo(video: Video) {
+    Swal.fire({
+      title: `${video.title}`,
+      width:'850px',
+      padding:'2.5rem',
+      background: '#F8F8F8',
+      position: 'center',
+      // grow:'fullscreen',
+      showClass: {
+        popup: 'swal2-show',
+        backdrop:'swal2-backdrop-show',
+      },
+      showCloseButton: true,
+      closeButtonHtml: '<i class="fas fa-times"></i>',
+      showConfirmButton:false,
+      html: 
+             `<iframe width="100%" 
+                     height="515" src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+                     frameborder="0" 
+                     allow="accelerometer; 
+                     autoplay; 
+                     clipboard-write; 
+                     encrypted-media; 
+                     gyroscope; 
+                     picture-in-picture" 
+                     allowfullscreen>
+              </iframe>`,
+     customClass: {
+       container:'swal2-container'
+     }
+       
+    })
+   
+    
+  }  
 
 }
